2166df0adfb915d569efa80a47c4f0283ee994a5
mveisoglu <mustafa.veisoglu@oneitesol.com>
2023-01-06 14:37:59 +0300

    ondo - redo doto game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [points, setPoints] = useState([]);
+  const [data, setData] = useState([]);
+
+  const clickHandle = (e) => {
+    setPoints((points) => [
+      ...points,
+      {
+        x: e.clientX,
+        y: e.clientY,
+      },
+    ]);
+  };
+  const redoHandle = (e) => {
+    e.stopPropagation();
+    const data = [...points];
+    const item = data.pop();
+    setData((data) => [...data, item]);
+    setPoints(data);
+  };
+  const undoHandle = (e) => {
+    e.stopPropagation();
+    const d = [...data];
+    const item = d.pop();
+    setPoints((points) => [...points, item]);
+    setData(d);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="screen" onClick={clickHandle}>
+      <header className="header">
+        <button disabled={points.length === 0} onClick={redoHandle}>
+          Redu
+        </button>
+        <button disabled={data.length === 0} onClick={undoHandle}>
+          Undo
+        </button>
       </header>
+      {points.map((point, key) => (
+        <div
+          className="point"
+          key={key}
+          style={{ "--left": point.x + "px", "--top": point.y + "px" }}
+        ></div>
+      ))}
     </div>
   );
 }
